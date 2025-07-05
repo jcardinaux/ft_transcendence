6a739e5e36ba8923827174203c9b2a0458d3925e
user <user@MacBook-Pro.local>
2025-07-05 12:09:42 +0200

    add: deprecated app (web-client + back&)

--- backend/web-client/src/app.ts
+++
@@ -1,9 +0,0 @@
-import { renderRoute } from "./router.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  renderRoute(window.location.pathname);
-
-  window.addEventListener("popstate", () => {
-    renderRoute(window.location.pathname);
-  });
-});
