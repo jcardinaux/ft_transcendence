99e9437fe15500a4bbad43c31ed01a4afdde7e21
jcardina <jcardina@student.42roma.it>
2025-07-10 15:58:40 +0200

    merge push

--- app/public/ts/main.ts
+++ app/public/ts/main.ts
@@ -1,5 +1,7 @@
 import { initRouter } from './Router.js';
+import { logInfo } from './utils/logger.js';
 
 window.addEventListener('DOMContentLoaded', () => {
+  logInfo('SPA started')
   initRouter();
 });
